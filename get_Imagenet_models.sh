1cf25d5b3c0e36f024aab2fa9cc9a7176d43e02b
tsborkar <tsborkar@asu.edu>
2018-01-06 00:08:29 -0700

    bash script to fetch trained models for ImageNet

---
+++ get_Imagenet_models.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+wget https://www.dropbox.com/s/8kzhrygp317oasb/Imagenet_models.tar.gz?dl=0
+
+mv Imagenet_models.tar.gz?dl=0 Imagenet_models.tar.gz
+
+tar -xvzf Imagenet_models.tar.gz
+
+rm -r *.gz
